c7529ab685eea1445f9084aa3de8e08bd199f8b6
Vyacheslav V <Vjacheslav.Vytjagov@smartbear.com>
2015-10-29 09:15:39 +0300

    Updated code to the new format of the responses

--- src/main/java/com/smartbear/aws/entity/HttpResourceDescription.java
+++ src/main/java/com/smartbear/aws/entity/HttpResourceDescription.java
@@ -2,7 +2,11 @@ package com.smartbear.aws.entity;
 
 import com.eviware.soapui.impl.rest.RestRequestInterface;
 import com.smartbear.aws.Helper;
+import com.smartbear.utils.JsonFormatter;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
 import java.util.Collections;
@@ -22,7 +26,7 @@ public final class HttpResourceDescription {
         this.path = src.getString("path", "");
         final String pathPart = src.getString("pathPart", "");
         this.name = pathPart.replace("/", "-");
-        this.methodsNames = this.findMethods(src.get("resourceMethods"));
+        this.methodsNames = this.findMethods(src);
     }
 
     @Override
@@ -30,17 +34,39 @@ public final class HttpResourceDescription {
         return String.format("%s, %s, %s, %s, %s\r\n", id, parentId, path, name, Helper.collectionToString(methodsNames));
     }
 
-    private List<String> findMethods(JsonValue src) {
+    private List<String> findMethods(JsonObject response) {
+        JsonArray methods = extractMethodsFromResponse(response);
         List<String> existingMethods = new LinkedList<>();
-        if (src instanceof JsonObject) {
-            JsonObject methodsObj = (JsonObject)src;
-            for (String method: RestRequestInterface.HttpMethod.getMethodsAsStringArray()) {
-                JsonValue methodJson = methodsObj.get(method);
-                if (methodJson instanceof JsonObject) {
-                    existingMethods.add(method);
-                }
+        for (JsonValue item: methods) {
+            if (item instanceof JsonObject) {
+                JsonObject methodJson = (JsonObject)item;
+                existingMethods.add(methodJson.getString("name"));
             }
         }
+
         return Collections.unmodifiableList(existingMethods);
+    }
+
+    private JsonArray extractMethodsFromResponse(JsonObject response) {
+        //IMPORTANT: _links and resource:methods fields are optional!!!
+        //IMPORTANT: resource:methods value can be an Object instead of an Array if child element is just one
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        if (!response.containsKey("_links")) {
+            return arrayBuilder.build();
+        }
+        JsonValue links = response.get("_links");
+        if (links instanceof JsonObject) {
+            JsonObject linksObject = (JsonObject)links;
+            if (!linksObject.containsKey("resource:methods")) {
+                return arrayBuilder.build();
+            }
+            JsonValue item = linksObject.get("resource:methods");
+            if (item instanceof JsonObject) {
+                return arrayBuilder.add(item).build();
+            } else if (item instanceof JsonArray) {
+                return (JsonArray)item;
+            }
+        }
+        return arrayBuilder.build();
     }
 }
